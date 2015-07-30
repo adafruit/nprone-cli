057fd369593328efa93db8162a724dd68f7ae72f
Todd Treece <toddtreece@gmail.com>
2015-07-30 17:38:39 -0400

    points test at getAccessToken

--- test/api.js
+++ test/api.js
@@ -1,26 +1,34 @@
 var API = require('../lib/api');
 
+process.env.NODE_ENV = 'test';
+
 describe('NPR API', function() {
+
+  this.timeout(10000);
 
   describe('OAuth', function() {
 
     it('should get an access token', function(done) {
 
-      var NPR = API({
+      var npr = API({
         client_id: process.env.CLIENT_ID,
         client_secret: process.env.CLIENT_SECRET,
         username: process.env.NPR_USERNAME,
         password: process.env.NPR_PASSWORD
       });
 
-      NPR.authenticate(function(token) {
+      npr.getAccessToken()
+        .then(function(token) {
 
-        if(! token)
-          return done('invalid token');
+          if(! token)
+            done('missing token');
 
-        done();
+          done();
 
-      });
+        })
+        .catch(function(err) {
+          done(err);
+        });
 
     });
 
