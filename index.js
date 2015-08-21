169bc60b0ea877ea3e92886e3c61938d371633a7
Todd Treece <toddtreece@gmail.com>
2015-08-21 22:59:31 +0000

    use new npr-api node lib

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-require('dotenv').load();
-
-var API = require('./lib/api'),
+var npr = require('npr-api')(),
     fs = require('fs'),
-    chalk = require('chalk');
-
-var npr = API({
-  client_id: process.env.CLIENT_ID,
-  client_secret: process.env.CLIENT_SECRET,
-  username: process.env.NPR_USERNAME,
-  password: process.env.NPR_PASSWORD
-});
+    chalk = require('chalk'),
+    auth = require('./lib/auth'),
+    omx = require('node-omx')();
+    wget = require('wget-improved');
 
 var logo = fs.readFileSync('./logo.txt', 'utf8');
 
@@ -18,13 +12,25 @@ console.log(logo);
 // silence swagger log output
 process.env.NODE_ENV = 'test';
 
-console.log(chalk.white.bold('User:', chalk.reset(npr.username)));
-
-npr.getRecommendation('npr')
-  .then(function(url) {
-    console.log(chalk.white.bold('Recommended Story:', chalk.reset(url)));
-  })
-  .catch(function(err) {
-    console.error(chalk.red.bold('ERROR',JSON.stringify(err)));
-  });
-
+npr.one
+   .init()
+   .then(auth.getToken.bind(auth, npr))
+   .then(function(token) {
+     return npr.one.setAccessToken(token);
+   })
+  .then(function() {
+     return npr.one.listening.getRecommendations({ channel: 'npr' });
+   })
+   .then(function(recommendations) {
+     // print out the first two recommendations to the console
+     var tmp = '/tmp/test.mp4 ';
+     wget.download(recommendations.items[0].links.audio[0].href, tmp)
+       .on('end', function() {
+         omx.play(tmp);
+         omx.on('play', function() {console.log('play');});
+         omx.on('stop', function() {console.log('stop');});
+       });
+   })
+   .catch(function(err) {
+     console.error(err,err.stack);
+   });
