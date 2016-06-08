a28bf3233cb23bbdae723a81430244ab776d9390
Todd Treece <toddtreece@gmail.com>
2016-06-08 08:17:17 -0400

    move auto update to bash script

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 'use strict';
 
 process.title = 'npr-one';
@@ -8,9 +7,7 @@ if(process.platform != 'linux' && process.platform != 'darwin') {
   process.exit(1);
 }
 
-const AutoUpdate = require('cli-autoupdate'),
-      exec = require('child_process').exec,
-      NPR = require('npr-api'),
+const NPR = require('npr-api'),
       chalk = require('chalk'),
       auth = require('./lib/auth'),
       fs = require('fs'),
@@ -19,55 +16,37 @@ const AutoUpdate = require('cli-autoupdate'),
       Story = require('./lib/story'),
       UI = require('./lib/ui');
 
-let pkg = require('./package.json');
-
 const logo = fs.readFileSync(path.join(__dirname,'logo.txt'), 'utf8');
-console.log(logo);
-console.log('checking for update...');
 
-if(process.platform == 'linux' && process.arch == 'arm')
-  exec('./install-service');
+const npr = new NPR(),
+      story = new Story(npr),
+      player = new Player();
 
-const update = new AutoUpdate(pkg);
+console.log('connecting to npr one...');
 
-update.on('error', (err) => { console.error('update failed.'); launch(); });
-update.on('update', () => console.log('updating...'));
-update.on('finish', () => launch());
+// silence swagger log output
+process.env.NODE_ENV = 'test';
 
-const launch = function() {
-
-  const npr = new NPR(),
-        story = new Story(npr),
-        player = new Player();
-
-  console.log('connecting to npr one...');
-
-  // silence swagger log output
-  process.env.NODE_ENV = 'test';
-
-  npr.one
-     .init()
-     .then(auth.getToken.bind(auth, npr))
-     .then((token) => {
-       process.stdout.write('\x1B[2J');
-       process.stdout.write('\x1B[0f');
-       console.log(logo);
-       return npr.one.setAccessToken(token);
-     })
-     .then(story.getRecommendations.bind(story))
-     .then(player.load.bind(player))
-     .then(() => {
-       const ui = new UI();
-       ui.on('skip', player.skip.bind(player));
-       ui.on('pause', player.pause.bind(player));
-       ui.on('rewind', player.rewind.bind(player));
-       ui.on('interesting', player.interesting.bind(player));
-       ui.on('volumeup', player.increaseVolume.bind(player));
-       ui.on('volumedown', player.decreaseVolume.bind(player));
-     })
-     .catch(function(err) {
-       console.error(err, err.stack);
-     });
-
-};
-
+npr.one
+   .init()
+   .then(auth.getToken.bind(auth, npr))
+   .then((token) => {
+     process.stdout.write('\x1B[2J');
+     process.stdout.write('\x1B[0f');
+     console.log(logo);
+     return npr.one.setAccessToken(token);
+   })
+   .then(story.getRecommendations.bind(story))
+   .then(player.load.bind(player))
+   .then(() => {
+     const ui = new UI();
+     ui.on('skip', player.skip.bind(player));
+     ui.on('pause', player.pause.bind(player));
+     ui.on('rewind', player.rewind.bind(player));
+     ui.on('interesting', player.interesting.bind(player));
+     ui.on('volumeup', player.increaseVolume.bind(player));
+     ui.on('volumedown', player.decreaseVolume.bind(player));
+   })
+   .catch(function(err) {
+     console.error(err, err.stack);
+   });
---
+++ version.js
@@ -0,0 +1,2 @@
+const pkg = require('./package.json');
+console.log(pkg.version);
